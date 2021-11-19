98632838f415162e3cfd360554d496ab69d20d75
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-18 17:17:57 -0700

    Save work on normalize_pkg.

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 normalize_pkg() {
-  local pkg="${1}"
+  # Strip any target specifications
+  local pkg="${1%:*}"
+  # Strip a trailing slash
+  local pkg="${pkg%/}"
+  # Strip a trailing slash
+  # Make sure to add prefix (//)
   case "${pkg}" in
     "//"*)
       ;;
