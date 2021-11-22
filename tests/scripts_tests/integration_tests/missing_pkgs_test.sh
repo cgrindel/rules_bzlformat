ff2367912bf33c57910878d31b5b6edf33ef8b94
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-22 14:19:57 -0700

    Save work.

--- tests/scripts_tests/integration_tests/missing_pkgs_test.sh
+++ tests/scripts_tests/integration_tests/missing_pkgs_test.sh
@@ -11,7 +11,54 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
 
-assertions_lib="$(rlocation cgrindel_bazel_shlib/lib/assertions.sh)"
+# DEBUG BEGIN
+set -x
+# DEBUG END
+
+# assertions_lib="$(rlocation cgrindel_bazel_shlib/lib/assertions.sh)"
+assertions_lib="$(rlocation cgrindel_bazel_shlib/lib/assertions.sh)" || (echo >&2 "Failed to locate cgrindel_bazel_shlib/lib/assertions.sh" && exit 1)
 source "${assertions_lib}"
+# [[ -f "${assertions_lib}" ]] && source "${assertions_lib}"
+# source "${assertions_lib}" || (echo >&2 "Failed to source ${assertions_lib}" && exit 1)
+
+paths_lib="$(rlocation cgrindel_bazel_shlib/lib/paths.sh)"
+source "${paths_lib}"
+
+messages_lib="$(rlocation cgrindel_bazel_shlib/lib/messages.sh)"
+source "${messages_lib}"
+
+# Process args
+while (("$#")); do
+  case "${1}" in
+    "--bazel")
+      bazel_rel_path="${2}"
+      shift 2
+      ;;
+    "--workspace")
+      workspace_path="${2}"
+      shift 2
+      ;;
+    "--bazel_cmd")
+      bazel_cmds+=("${2}")
+      shift 2
+      ;;
+    *)
+      shift 1
+      ;;
+  esac
+done
+
+[[ -n "${bazel_rel_path:-}" ]] || exit_with_msg "Must specify the location of the Bazel binary."
+[[ -n "${workspace_path:-}" ]] || exit_with_msg "Must specify the location of the workspace file."
+
+starting_path="$(pwd)"
+starting_path="${starting_path%%*( )}"
+bazel="$(normalize_path "${bazel_rel_path}")"
+
+workspace_dir="$(dirname "${workspace_path}")"
+cd "${workspace_dir}"
+
+
+
 
 fail "IMPLEMENT ME!"
