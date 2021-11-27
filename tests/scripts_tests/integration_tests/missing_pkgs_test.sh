4929013805041f801ca0cb73691c29804128def0
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-27 10:28:19 -0700

    Created scratch directory and copying files to it.

--- tests/scripts_tests/integration_tests/missing_pkgs_test.sh
+++ tests/scripts_tests/integration_tests/missing_pkgs_test.sh
@@ -58,25 +58,115 @@ starting_path="$(pwd)"
 starting_path="${starting_path%%*( )}"
 bazel="$(normalize_path "${bazel_rel_path}")"
 
-workspace_dir="$(dirname "${workspace_path}")"
+# workspace_dir="$(dirname "${workspace_path}")"
+workspace_dir="$(normalize_path "$(dirname "${workspace_path}")")"
 cd "${workspace_dir}"
 
-# TODO: Backup files that are modified and restore on cleanup.
+# DEBUG BEGIN
+echo >&2 "*** CHUCK  starting_path: ${starting_path}" 
+echo >&2 "*** CHUCK  workspace_dir: ${workspace_dir}" 
+echo >&2 "*** CHUCK WORKSPACE DIR" 
+tree
+# DEBUG END
+
+# MARK - Backup and Restore Workspace Files
+
+# files_to_backup=(BUILD.bazel foo/BUILD.bazel foo/bar/BUILD.bazel)
+
+# backup_filename() {
+#   local file="${1}"
+#   echo "${file}.BAK"
+# }
+
+# backup_file() {
+#   local file="${1}"
+#   local backup_file="$(backup_filename "${file}")"
+#   # [[ -f "${backup_file}" ]] && exit_with_msg "Backup file already exists. ${backup_file}"
+#   if [[ -L "${file}" ]]; then
+#     # For symlinks, rename the file and then copy it back.
+#     mv -f "${file}" "${backup_file}"
+#     restore_file "${file}"
+#   else
+#     cp -f "${file}" "${backup_file}"
+#   fi
+# }
+
+# backup_files() {
+#   files=()
+#   while (("$#")); do
+#     case "${1}" in
+#       *)
+#         files+=("${1}")
+#         shift 1
+#         ;;
+#     esac
+#   done
+#   [[ ${#files[@]} == 0 ]] && files=( "${files_to_backup[@]}" )
+#   for file in "${files[@]}" ; do
+#     backup_file "${file}"
+#   done
+# }
+
+# restore_file() {
+#   local file="${1}"
+#   local backup_file="$(backup_filename "${file}")"
+#   cp -f "${backup_file}" "${file}"
+# }
+
+# restore_files() {
+#   # DEBUG BEGIN
+#   set -x
+#   # DEBUG END
+#   files=()
+#   while (("$#")); do
+#     case "${1}" in
+#       *)
+#         files+=("${1}")
+#         shift 1
+#         ;;
+#     esac
+#   done
+#   [[ ${#files[@]} == 0 ]] && files=( "${files_to_backup[@]}" )
+#   for file in "${files[@]}" ; do
+#     restore_file "${file}"
+#   done
+# }
+
+# backup_files
+
+# cleanup() {
+#   # DEBUG BEGIN
+#   echo >&2 "*** CHUCK CLEANUP" 
+#   # DEBUG END
+#   restore_files
+# }
+# trap cleanup EXIT
+
+# MARK - Create Scratch Directory
+
+# Create the scratch directory
+# scratch_dir="$(mktemp -d -t missing_pkgs_test)" || exit_with_msg "Failed to create a scratch directory."
+# scratch_dir="${workspace_dir}/../scratch"
+scratch_dir="$(normalize_path "${workspace_dir}/../scratch")"
+rm -rf "${scratch_dir}"
+mkdir -p "${scratch_dir}"
+
+# Copy the non-hidden files
+cp -R -L * "${scratch_dir}"
+
+# Copy the hidden files
+find . -type f -name ".*" -print0 | xargs -0 -I '{}' cp '{}' "${scratch_dir}"
+
+cd "${scratch_dir}"
+
+# DEBUG BEGIN
+echo >&2 "*** CHUCK CREATED SCRATCH WORKSPACE" 
+tree -a "${scratch_dir}"
+# DEBUG END
 
 # MARK - Find the missing packages
 
 missing_pkgs=( $("${bazel}" run "//:bzlformat_pkgs_find_missing") )
-
-# assert_msg="Missing packages count, no exclusions."
-# expected_array=(// //foo //foo/bar)
-# assert_equal 3 ${#missing_pkgs[@]} "${assert_msg}"
-# for (( i = 0; i < ${#expected_array[@]}; i++ )); do
-#   assert_equal "${expected_array[${i}]}" "${missing_pkgs[${i}]}" "${assert_msg}[${i}]"
-# done
-
-# assert_equal "//" "${missing_pkgs[0]}" "${assert_msg}[0]"
-# assert_equal "//foo" "${missing_pkgs[1]}" "${assert_msg}[1]"
-# assert_equal "//foo/bar" "${missing_pkgs[2]}" "${assert_msg}[2]"
 
 assert_msg="Missing packages, no exclusions"
 expected_array=(// //foo //foo/bar)
@@ -91,17 +181,30 @@ done
 "${buildozer}" 'add exclude //foo' //:bzlformat_pkgs
 
 missing_pkgs=( $("${bazel}" run "//:bzlformat_pkgs_find_missing") )
-assert_equal 2 ${#missing_pkgs[@]} "Missing packages count, with exclusions."
-assert_equal "//" "${missing_pkgs[0]}" "Missing packages 0"
-assert_equal "//foo/bar" "${missing_pkgs[1]}" "Missing packages 1"
+assert_msg="Missing packages, with exclusions"
+expected_array=(// //foo/bar)
+assert_equal ${#expected_array[@]} ${#missing_pkgs[@]} "${assert_msg}"
+for (( i = 0; i < ${#expected_array[@]}; i++ )); do
+  assert_equal "${expected_array[${i}]}" "${missing_pkgs[${i}]}" "${assert_msg}[${i}]"
+done
 
 # MARK - Update the missing packages with exclusions
 
 update_pkgs=( $("${bazel}" run "//:bzlformat_pkgs_update_missing") )
-assert_equal 3 ${#missing_pkgs[@]} "Updated packages count, with exclusions."
-assert_equal "Updating the following packages:" "${missing_pkgs[0]}" "Missing text."
-assert_equal "//" "${missing_pkgs[1]}" "Missing packages 1"
-assert_equal "//foo/bar" "${missing_pkgs[2]}" "Missing packages 2"
+# DEBUG BEGIN
+echo >&2 "*** CHUCK  update_pkgs:"
+for (( i = 0; i < ${#update_pkgs[@]}; i++ )); do
+  echo >&2 "*** CHUCK   ${i}: ${update_pkgs[${i}]}"
+done
+# DEBUG END
+assert_msg="Update missing packages, with exclusions"
+expected_array=("Updating the following packages:" // //foo/bar)
+assert_equal ${#expected_array[@]} ${#update_pkgs[@]} "${assert_msg}"
+for (( i = 0; i < ${#expected_array[@]}; i++ )); do
+  assert_equal "${expected_array[${i}]}" "${update_pkgs[${i}]}" "${assert_msg}[${i}]"
+done
 
 
+# DEBUG BEGIN
 fail "IMPLEMENT ME!"
+# DEBUG END
